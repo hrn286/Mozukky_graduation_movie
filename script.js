6f18b287324935ccdfe847be2387bab1473ce465
hrn286 <haru020806@gmail.com>
2026-03-20 11:04:46 +0900

    Add functions to navigate steps and play video

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function goStep2() {
+  document.getElementById("step1").classList.add("hidden");
+  document.getElementById("step2").classList.remove("hidden");
+}
 
+function goStep3() {
+  document.getElementById("step2").classList.add("hidden");
+  document.getElementById("step3").classList.remove("hidden");
+}
+
+function playVideo() {
+  const videoArea = document.getElementById("videoArea");
+  const frame = document.getElementById("youtubeFrame");
+
+  frame.src = "https://www.youtube.com/embed/YOUR_YOUTUBE_VIDEO_ID?autoplay=1";
+  videoArea.classList.remove("hidden");
+}
